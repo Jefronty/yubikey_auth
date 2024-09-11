2c195bbf8945ab0ebd2012a37cd063588f0b658f
Jefronty <matt@odetrix.com>
2024-09-11 10:52:28 -0400

    Create yubi.py

---
+++ yubi.py
@@ -0,0 +1,10 @@
+"""
+client_id and key values are acquired from
+https://upgrade.yubico.com/getapikey/
+
+known_devices is a tuple of the identifying substring of a yubikey token, the first 12 characters
+ie: 'cccccccc0123'
+"""
+client_id = 'client ID'
+key = 'unique_API_key'
+known_devices = ()
