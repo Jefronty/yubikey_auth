115e6dcc6c881ca138578d8a967fabc2e7e44bfd
Jefronty <matt@odetrix.com>
2024-09-19 12:04:32 -0400

    added verbose option to CLI

--- yubikey_check.py
+++ yubikey_check.py
@@ -3,15 +3,13 @@ import sys
 from yubico_client import Yubico
 try:
 	from inputimeout import inputimeout
-except:
-	pass
-if 'inputimeout' in locals():
 	has_ito = True
-else:
+except:
 	has_ito = False
 
 class YubiCheck(object):
 	"""class to verify Yubikey token"""
+	_response = None
 	def __init__(self, client_id=None, key=None):
 		"""initialize API"""
 		self.known_devices = yubi.known_devices
@@ -20,6 +18,9 @@ class YubiCheck(object):
 		if not key:
 			key = yubi.key
 		self.client = Yubico(client_id, key)
+
+	def response(self):
+		return self._response
 
 	def yubi_check(self, inp=None, strict=True):
 		"""check yubikey token for validity
@@ -40,31 +41,45 @@ class YubiCheck(object):
 			else:
 				try:
 					inp = inputimeout('Touch Yubikey ', 30)
-				except:
+				except Exception as e:
 					# no input provided
+					self._response = {'status': 'Failed', 'description': 'No token string provided', 'error': e}
 					return False
-		if len(inp.strip()) != 44:
+		try:
+			inp = inp.lower().strip()
+		except:
+			self._response = {'status': 'Failed', 'description': 'non-string token value provided'}
+			return False
+		if len(inp) != 44:
 			# invalid input
+			self._response = {'status': 'Failed', 'description': 'invalid token string provided'}
 			return False
 		if strict and len(self.known_devices) > 0 and inp[:12] not in self.known_devices:
+			self._response = {'status': 'Failed', 'description': 'unauthorized key used'}
 			return False
 		try:
-			return self.client.verify(inp.strip())
-		except:
+			self._response = self.client.verify(inp, return_response=True)
+			return self._response['status'] == 'OK'
+		except Exception as e:
+			self._response = {'status': 'Failed', 'description': 'API Error', 'error': e}
 			return False
 
 if __name__ == "__main__":
-    import argparse
-    parser = argparse.ArgumentParser(description="Standalone script for verifying Yubikey touch")
-    parser.add_argument('token_str', nargs='?', help='output of Yubikey without flag')
-    parser.add_argument('-c', '--client', help='API client_id string')
-    parser.add_argument('-k', '--key', help='API secret_key string')
-    parser.add_argument('-t', '--token', help='output of Yubikey')
-    args = parser.parse_args()
+	import argparse
+	parser = argparse.ArgumentParser(description="Standalone script for verifying Yubikey touch")
+	parser.add_argument('token_str', nargs='?', help='output of Yubikey without flag')
+	parser.add_argument('-c', '--client', metavar='CID', help='API client_id string')
+	parser.add_argument('-k', '--key', help='API secret_key string')
+	parser.add_argument('-t', '--token', metavar='STR', help='output of Yubikey')
+	parser.add_argument('-V', '--verbose', action='store_true', help='display full response')
 
-    token = args.token or args.token_str
-    cid = args.client
-    key = args.key
+	args = parser.parse_args()
 
-    yc = YubiCheck(cid, key)
-    print(str(yc.yubi_check(token)))
+	token = args.token or args.token_str
+	cid = args.client
+	key = args.key
+
+	yc = YubiCheck(cid, key)
+	print(str(yc.yubi_check(token)))
+	if args.verbose:
+		print(yc.response())
